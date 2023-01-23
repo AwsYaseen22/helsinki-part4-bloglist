1bd290db414648cb8e7a0dbea2018c1f1cf54f61
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-01-23 22:17:19 +0300

    4.18: bloglist expansion, step6

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const app = express()
 
 const mongoUrl = config.MONGODB_URI
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
